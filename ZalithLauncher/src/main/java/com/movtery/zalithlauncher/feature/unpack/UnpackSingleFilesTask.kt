1deb20ef3805be3d650260e64eac2093d76f2fe2
redcarl <carl030101@outlook.com>
2024-11-20 16:43:04 +0800

    fix(unpack): 补充复制 launcher_profiles.json 文件
    - 在 UnpackSingleFilesTask 中添加了复制 launcher_profiles.json 文件的操作
    -将文件复制到 gameHome 目录下，且不覆盖现有文件

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/feature/unpack/UnpackSingleFilesTask.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/feature/unpack/UnpackSingleFilesTask.kt
@@ -1,6 +1,7 @@
 package com.movtery.zalithlauncher.feature.unpack
 
 import android.content.Context
+import com.movtery.zalithlauncher.feature.customprofilepath.ProfilePathHome.Companion.gameHome
 import com.movtery.zalithlauncher.feature.log.Logging.e
 import com.movtery.zalithlauncher.utils.CopyDefaultFromAssets.Companion.copyFromAssets
 import com.movtery.zalithlauncher.utils.PathAndUrlManager
@@ -12,6 +13,7 @@ class UnpackSingleFilesTask(val context: Context) : AbstractUnpackTask() {
     override fun run() {
         runCatching {
             copyFromAssets(context)
+            Tools.copyAssetFile(context, "launcher_profiles.json", gameHome, false)
             Tools.copyAssetFile(context, "servers.json", PathAndUrlManager.DIR_GAME_HOME, true)
             Tools.copyAssetFile(context, "resolv.conf", PathAndUrlManager.DIR_DATA, false)
         }.getOrElse { e("AsyncAssetManager", "Failed to unpack critical components !") }
