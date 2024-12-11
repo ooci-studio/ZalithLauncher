0173bb4e6310496ce9b59b5d1d75851acfdf43be
redcarl <carl030101@outlook.com>
2024-12-11 16:48:11 +0800

    refactor(ZalithLauncher): 更新包名和应用名称
    -将包名从 com.movtery.zalithlauncher 改为 co.ooci.zalithlauncher
    -将应用名称从 "Zalith Launcher" 改为 "Ooci Launcher"

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/InfoCenter.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/InfoCenter.kt
@@ -4,8 +4,8 @@ import android.content.Context
 
 class InfoCenter {
     companion object {
-        const val LAUNCHER_NAME: String = "ZalithLauncher"
-        const val APP_NAME: String = "Zalith Launcher"
+        const val LAUNCHER_NAME: String = "OociLauncher"
+        const val APP_NAME: String = "Ooci Launcher"
 
         @JvmStatic
         fun replaceName(context: Context, resString: Int): String = context.getString(resString, APP_NAME)
